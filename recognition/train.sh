6530a274aa27458d3842e6c0b5d20ba9510c5618
dypromise <nanhangdingyang@163.com>
2019-03-27 22:20:12 +0800

    1080ti modify

--- recognition/train.sh
+++ recognition/train.sh
@@ -1,16 +1,15 @@
-export MXNET_CPU_WORKER_NTHREADS=6
+export MXNET_CPU_WORKER_NTHREADS=16
 export MXNET_ENGINE_TYPE=ThreadedEnginePerDevice
 
-export PATH="/home/dingyang/anaconda2/bin:$PATH"
-export LD_LIBRARY_PATH="/home/dingyang/anaconda2/lib/:/home/dingyang/anaconda3/lib:$LD_LIBRARY_PATH"
-
+#export PATH="/home/dingyang/anaconda2/bin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/cuda-9.0/lib64:$LD_LIBRARY_PATH"
 python2 -u train.py \
     --network r50 \
     --loss arcface \
     --dataset emore \
-    --pretrained /home/dingyang/r50-arcface-emore_ckpt_wo_fc7/model \
-    --pretrained-epoch 1 \
-    --verbose 2 \
-    --per-batch-size 64 \
-    --lr 0.05 \
-#    > train.log 2>&1
+    --pretrained /home/xmmtyding/model-r50-am-lfw/model \
+    --pretrained-epoch 0 \
+    --verbose 1000 \
+    --per-batch-size 100 \
+    --lr 0.0001 \
+    > train_paper_model_goon_lr0.0001.log 2>&1
