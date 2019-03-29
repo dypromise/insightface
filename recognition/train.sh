8d413b7863303fe1711d8fc9dabbe66793175cd5
dypromise <nanhangdingyang@163.com>
2019-03-30 03:52:29 +0800

    moodify train.sh

--- recognition/train.sh
+++ recognition/train.sh
@@ -7,10 +7,10 @@ export LD_LIBRARY_PATH="/usr/local/cuda-9.0/lib64:$LD_LIBRARY_PATH"
 python2 -u train.py \
     --network r50 \
     --loss arcface \
-    --dataset emore \
-    --pretrained /home/xmmtyding/model-r50-am-lfw/model \
+    --dataset emore_celebasian \
+    --pretrained /data1/xmmtyding/r50_arcface_emore_finetune_lr0.0001fc7mul10_wofc7/model \
     --pretrained-epoch 1 \
     --verbose 1000 \
-    --per-batch-size 64 \
-    --lr 0.0001
-#    > train.log 2>&1
+    --per-batch-size 96 \
+    --lr 0.0001 \
+    > train_emore_celebasian_finetune.log 2>&1
